3f655a001ee087e73b806fca146f1b56e11322ea
Jonas Raedle <jonas.raedle@gmail.com>
2012-11-11 16:37:48 +0100

    Finished makeMove method and added a test for it.

--- src/othello/Othello.java
+++ src/othello/Othello.java
@@ -3,26 +3,26 @@ import java.util.ArrayList;
 
 public class Othello {
 	//Number of moves made. == number of tokens - 4
-	private int num_moves;
+	private int numMoves;
 	
 	//0: not occupied 1: white 2: black
 	private int[][] board;
 	
-	private int white_tokens;
-	private int black_tokens;
+	private int whiteTokens;
+	private int blackTokens;
 	
 	private final int[][] directions = { {0,1}, {-1,1}, {-1,0}, {-1,-1}, {0,-1}, {1,-1}, {1,0}, {1,1} };
 	
-	public int getWhite_tokens() {
-		return white_tokens;
+	public int getWhiteTokens() {
+		return whiteTokens;
 	}
 
-	public int getBlack_tokens() {
-		return black_tokens;
+	public int getBlackTokens() {
+		return blackTokens;
 	}
 	
 	public int getNumMoves() {
-		return num_moves;
+		return numMoves;
 	}
 	
 	public Othello() {
@@ -35,8 +35,8 @@ public class Othello {
 		board[4][4] = 1;
 		board[3][4] = 2;
 		board[4][3] = 2;
-		num_moves = 0;
-		white_tokens = black_tokens = 2;
+		numMoves = 0;
+		whiteTokens = blackTokens = 2;
 	}
 	
 	public int getValue(Move move) {
@@ -47,14 +47,17 @@ public class Othello {
 		if (getValue(move) != 0) return false;
 		int opponent = (player == 1) ? 2 : 1;
 		for(int[] direction : directions) {
-			int x = move.x; int y = move.y;
-			x += direction[0]; y += direction[1];
-			if (x >= 8 || x < 0 || y >= 8 || y < 0 || 
-					board[x][y] != opponent) continue;
-			x += direction[0]; y += direction[1];
+			int x = move.x + direction[0];
+			int y = move.y + direction[1];
+			boolean turned = false;
 			while (x < 8 && x >= 0 && y < 8 && y >= 0) {
-				if (board[x][y] == 0) break;
-				else if (board[x][y] == player) return true;
+				if (board[x][y] == 0)
+					break;
+				else if (board[x][y] == opponent)
+					turned = true;
+				else if(board[x][y] == player && !turned)
+					break;
+				else return true;
 				x += direction[0]; y += direction[1];
 			}
 		}
@@ -72,12 +75,49 @@ public class Othello {
 		return moves;
 	}
 	
+	public void makeMove(int player, Move move) {
+		if (player != 1 && player != 2) 
+			throw new IllegalArgumentException("Player has to be 1 or 2; not " + player);
+		if (!isValidMove(player, move))
+			throw new IllegalArgumentException("Move (" + move.x + "," + move.y + ") is not valid!");
+
+		board[move.x][move.y] = player;
+		if (player == 1)  whiteTokens++; else blackTokens++;
+		numMoves++;
+		int opponent = player == 1 ? 2 : 1;
+		
+		
+		for (int[] direction : directions) {
+			ArrayList<Move> turns = new ArrayList<Move>();
+			int x = move.x + direction[0];
+			int y = move.y + direction[1];
+			while(x < 8 && x >= 0 && y < 8 && y >= 0) {
+				if (board[x][y] == opponent)
+					turns.add(new Move(x,y));
+				else if (board[x][y] == player && !turns.isEmpty())
+					turn(player, turns);
+				else break;
+				x += direction[0]; y += direction[1];
+			}
+		}
+	}
+
+	private void turn(int player, ArrayList<Move> turns) {
+		for (Move turn : turns) {
+			if (board[turn.x][turn.y]!= player) {
+				board[turn.x][turn.y] = player;
+				if (player == 1) whiteTokens++; else whiteTokens--;
+				if (player == 1) blackTokens--; else blackTokens++;
+			}
+		}
+	}
+	
 	@Override
 	public String toString() {
 		String s = "";
 		for (int i = 0; i < 8; i++) {
 			for (int n = 0; n < 8; n++) {
-				s += board[i][n] + " ";
+				s += board[n][i] + " ";
 			}
 			s += "\n";
 		}
--- test/othello/OthelloTest.java
+++ test/othello/OthelloTest.java
@@ -12,10 +12,28 @@ public class OthelloTest {
 		Othello o = new Othello();
 		ArrayList<Move> moves = o.getValidMoves(2);
 		assertTrue("Number of valid moves is not 4!", moves.size() == 4);
-		assertTrue("Move(2,3) missing!", moves.contains(new Move(2,3)));
-		assertTrue("Move missing!", moves.contains(new Move(3,2)));
-		assertTrue("Move missing!", moves.contains(new Move(4,5)));
-		assertTrue("Move missing!", moves.contains(new Move(5,4)));
+		assertTrue("Move (2,3) missing!", moves.contains(new Move(2,3)));
+		assertTrue("Move (3,2) missing!", moves.contains(new Move(3,2)));
+		assertTrue("Move (4,5) missing!", moves.contains(new Move(4,5)));
+		assertTrue("Move (5,4) missing!", moves.contains(new Move(5,4)));
+	}
+	@Test
+	public void testMakeMoves() {
+		Othello o = new Othello();
+		o.makeMove(2, new Move(3,2));
+		o.makeMove(1, new Move(2,2));
+		o.makeMove(2, new Move(4,5));
+		o.makeMove(1, new Move(5,5));
+		assertTrue("Incorrect num of white tokens (" + o.getWhiteTokens() + ")"
+				, o.getWhiteTokens() == 4);
+		assertTrue("Incorrect num of black tokens (" + o.getBlackTokens() + ")"
+				, o.getBlackTokens() == 4);
+		ArrayList<Move> moves = o.getValidMoves(2);
+		assertTrue("Number of valid moves is not 4!", moves.size() == 4);
+		assertTrue("Move (1,2) missing!", moves.contains(new Move(1,2)));
+		assertTrue("Move (2,3) missing!", moves.contains(new Move(2,3)));
+		assertTrue("Move (5,4) missing!", moves.contains(new Move(5,4)));
+		assertTrue("Move (6,5) missing!", moves.contains(new Move(6,5)));
 	}
 
 }
